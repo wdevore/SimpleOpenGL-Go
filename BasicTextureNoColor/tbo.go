ad25a0d33ce1205f627f24c4e6a263f45d456d5d
William Cleveland <iposthuman@pop-os.localdomain>
2020-07-07 18:44:51 -0400

    finished BasicTextureNoColor example

---
+++ BasicTextureNoColor/tbo.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"image"
+	"image/draw"
+	_ "image/png" // For 'png' images
+	"os"
+
+	"github.com/go-gl/gl/v4.5-core/gl"
+)
+
+// makeTbo initializes
+func makeTbo() uint32 {
+	var tbo uint32
+	gl.GenTextures(1, &tbo)
+	return tbo
+}
+
+func bindTexture(tbo uint32) {
+	gl.ActiveTexture(gl.TEXTURE0)
+	gl.BindTexture(gl.TEXTURE_2D, tbo)
+}
+
+func bindTbo(texture *image.NRGBA, tbo uint32) {
+	gl.ActiveTexture(gl.TEXTURE0)
+	gl.BindTexture(gl.TEXTURE_2D, tbo)
+
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+
+	width := int32(texture.Bounds().Dx())
+	height := int32(texture.Bounds().Dy())
+
+	// gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT)
+	// gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT)
+
+	// Give the image to OpenGL
+	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(texture.Pix))
+	gl.GenerateMipmap(gl.TEXTURE_2D)
+}
+
+func loadImage(path string) (*image.NRGBA, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+
+	bounds := img.Bounds()
+	nrgba := image.NewNRGBA(image.Rect(0, 0, bounds.Dx(), bounds.Dy()))
+
+	r := image.Rect(0, 0, bounds.Dx(), bounds.Dy())
+	flippedImg := image.NewNRGBA(r)
+
+	// Transfer data to image
+	draw.Draw(nrgba, nrgba.Bounds(), img, bounds.Min, draw.Src)
+
+	// Flip horizontally or around Y-axis
+	// for j := 0; j < nrgba.Bounds().Dy(); j++ {
+	// 	for i := 0; i < nrgba.Bounds().Dx(); i++ {
+	// 		flippedImg.Set(bounds.Dx()-i, j, nrgba.At(i, j))
+	// 	}
+	// }
+
+	// Flip vertically or around the X-axis
+	for j := 0; j < nrgba.Bounds().Dy(); j++ {
+		for i := 0; i < nrgba.Bounds().Dx(); i++ {
+			flippedImg.Set(i, bounds.Dy()-j, nrgba.At(i, j))
+		}
+	}
+
+	return flippedImg, nil
+}
---
+++ BasicTextureNoColor/vbo.go
@@ -0,0 +1,54 @@
+package main
+
+import "github.com/go-gl/gl/v4.5-core/gl"
+
+// makeVbo initializes and returns a vertex buffer object from the points provided.
+func makeVbo() uint32 {
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+
+	return vbo
+}
+
+func bindVbo(points []float32, vbo uint32) {
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
+
+	// Specify the vertex attribute layout. This specifies--how during transmission--opengl
+	// will pass the data to the shader and how the shader will extract the data.
+	vertexInputAttrb := uint32(0)
+	sizeOfInputAttrb := int32(3)
+	stride := int32(0)
+	gl.VertexAttribPointer(vertexInputAttrb, sizeOfInputAttrb, gl.FLOAT, false, stride, nil)
+	gl.EnableVertexAttribArray(vertexInputAttrb)
+}
+
+func bindTextureVbo(points []float32, vbo uint32) {
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
+
+	sizeOfFloat := int32(4)
+
+	// If the data per-vertex is (x,y,z,s,t = 5) then
+	// Stride = 5 * size of float
+	// OR
+	// If the data per-vertex is (x,y,z,r,g,b,s,t = 8) then
+	// Stride = 8 * size of float
+
+	// Our data layout is x,y,z,s,t
+	stride := 5 * sizeOfFloat
+
+	// position attribute
+	size := int32(3)   // x,y,z
+	offset := int32(0) // position is first thus this attrib is offset by 0
+	attribIndex := uint32(0)
+	gl.VertexAttribPointer(attribIndex, size, gl.FLOAT, false, stride, gl.PtrOffset(int(offset)))
+	gl.EnableVertexAttribArray(0)
+
+	// texture coord attribute is offset by 3 (i.e. x,y,z)
+	size = int32(2)   // s,t
+	offset = int32(3) // the preceeding component size = 3, thus this attrib is offset by 3
+	attribIndex = uint32(1)
+	gl.VertexAttribPointer(attribIndex, size, gl.FLOAT, false, stride, gl.PtrOffset(int(offset*sizeOfFloat)))
+	gl.EnableVertexAttribArray(1)
+}
